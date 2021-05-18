644d89416f4dd57023f0672fd01f683ae2900370
jasonjoby <jason.joby072@gmail.com>
2021-05-19 01:48:05 +0530

    first commit

---
+++ Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int y = 0;
+    
+    while (1){
+        cin>>y;
+        if (y !=42) {cout<<y<<endl;}
+        else break;
+    }
+
+}
